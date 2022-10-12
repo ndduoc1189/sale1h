971767e15339c765f495b07f813839b94d42e171
ndduoc <ndduoc1189@gmail.com>
2022-10-12 15:58:14 +0700

    up 2

--- sale1h-api/src/config/index.js
+++ sale1h-api/src/config/index.js
@@ -6,7 +6,9 @@ const config = {
   sendoProductAPI: "https://searchlist-api.sendo.vn/web/products",
   afURL: "https://go.isclix.com/deep_link/4920806040439407239?utm_source=sale1h&url=",
   requestHeader:{
-      "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
   
 }
