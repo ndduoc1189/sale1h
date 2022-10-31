8680bf333af85741454220b2cb479735c74ebff5
NDDUOC-LAP <ndduoc1189@gmail.com>
2022-11-01 06:26:10 +0700

    save

--- api/server.js
+++ api/server.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const app = express();
-const PORT = process.env.PORT || 7901
-
-app.use(helmet());
-app.use(morgan('common'));
-
-app.get('/',(req, res,next ) => {
-    res.send('this is a test');
-})
+const app = require('./src/app');
 
 app.listen(PORT,()=>{
      console.log('Start on port: '+PORT);
